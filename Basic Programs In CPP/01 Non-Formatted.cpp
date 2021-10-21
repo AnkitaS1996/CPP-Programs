c6d1e435ec5d84deb56d8e4048f90d815f4ec3af
AnkitaS1996 <ankitashedage2661@gmail.com>
2021-10-21 23:18:53 +0530

    NonFormatted Program CPP

---
+++ Basic Programs In CPP/01 Non-Formatted.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    cout<<" Ankita \t";
+    cout<<"95 \t";
+    cout<<"100 \t";
+    cout<<"195 \t";
+    cout<<" 96.6 \t"<<endl;
+    cout<<" Piya \t";
+    cout<<" 100 \t";
+    cout<<" 80 \t";
+    cout<<"185 \t";
+    cout<<"896.77";
+
+    _getch();
+    return 0;
+
+}
+
