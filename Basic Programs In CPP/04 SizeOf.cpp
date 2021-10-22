ab79802b692c74fe7ee2af60a9a5e39a60254a36
AnkitaS1996 <ankitashedage2661@gmail.com>
2021-10-22 16:11:14 +0530

    SizeOf Program

---
+++ Basic Programs In CPP/04 SizeOf.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int No;
+   char Ch;
+    float f;
+    double d;
+    long double ld;
+
+    cout<<"Sizeof integer = "<<sizeof(No)<<endl;
+    cout<<"Sizeof character = "<<sizeof(Ch)<<endl;
+    cout<<"Sizeof float = "<<sizeof(f)<<endl;
+    cout<<"Sizeof long Double = "<<sizeof(ld)<<endl;
+    cout<<"Sizeof Double = "<<sizeof(d)<<endl;
+    _getch();
+    return 0;
+}
