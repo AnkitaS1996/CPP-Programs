23cba93c024fc37848d51316a01a2ea340715f47
AnkitaS1996 <ankitashedage2661@gmail.com>
2021-11-03 17:14:11 +0530

    ASCII Character To ASCII Value Program

---
+++ Basic Programs In CPP/07 ASCII Character to ASCII Value.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    char Ch;
+
+    cout<<"\n Enter Character to gets its ASCII Value : ";
+    cin>>Ch;
+
+    cout<<"\n Equivalent Character is "<<Ch<<" = "<<int(Ch);
+    cout<<"\n\n Thank You"<<endl;
+
+    _getch();
+    return 0;
+}
