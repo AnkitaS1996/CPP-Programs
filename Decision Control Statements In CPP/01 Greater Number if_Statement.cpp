2bc05dfd000097129e648bf033cc7e5561055133
AnkitaS1996 <ankitashedage2661@gmail.com>
2021-12-07 00:04:34 +0530

    Greater Number if else statements

---
+++ Decision Control Statements In CPP/01 Greater Number if_Statement.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int iNo1 = 0;
+    int iNo2 = 0;
+    cout<<"\n Enter Any Two Numbers : ";
+    cin>>iNo1>>iNo2;
+    if(iNo1 > iNo2)
+    {
+        cout<<iNo1<<" "<<"Number is Greater";
+    }
+    cout<<"\n Thank You";
+
+    _getch();
+    return 0;
+}
