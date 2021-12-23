cffaa10e50ef944faf482bc1db055c578c6c7b56
AnkitaS1996 <ankitashedage2661@gmail.com>
2021-12-23 22:01:06 +0530

    Basic Program For Loop

---
+++ Loop Statements/01 Welcome Print 10 times Using For Loop.cpp
@@ -0,0 +1,14 @@
+// //////////////////////////For Loop --> Welcome Print 10 times //////////////////////////////////////////
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int i = 0;
+    for(i=0;i<10;i++)
+    {
+        cout<<"\n Welcome";
+    }
+    _getch();
+    return 0;
+}
